69f369ee1b271c560613750e595d96387fa82081
Bassel Ashi <45138315+BasselAshi@users.noreply.github.com>
2020-01-23 02:10:19 -0500

    Updated image_editor.c
    Added comments for reference

--- image_editor.c
+++ image_editor.c
@@ -11,12 +11,10 @@ void convert_to_black_and_white();
 void instagram_square();
 
 int main(int argc, char *argv[]) {
-    /* Task 1 needs to be completed here: Make sure that the command line arguments are correct. In case an error exists,
-     * print the error message and terminate the program. Otherwise, invoke the corresponding
-     * image processing function. */
 
     int choice = strtol(argv[1], NULL, 10);
 
+    // Command arguments determine the option
     if (choice == 1 && argc == 2) {
         remove_red();
     } else if (choice == 2 && argc == 2) {
